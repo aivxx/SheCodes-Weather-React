379dcfe2d4c1922a4fcc1c94025c3da83f506228
aivxx <90653195+aivxx@users.noreply.github.com>
2022-01-09 14:41:28 -0500

    clean up app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import logo from "./logo.svg";
+import React from "react";
+
 import "./App.css";
 
-function App() {
+export default function App() {
   return <div className="App">Weather App</div>;
 }
-
-export default App;
