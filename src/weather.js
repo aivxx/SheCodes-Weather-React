cfb7e0ee0ebc42163409abcbb2cfb7d168d2d164
aivxx <90653195+aivxx@users.noreply.github.com>
2022-01-09 18:42:01 -0500

    style weather

--- src/weather.js
+++ src/weather.js
@@ -5,13 +5,24 @@ export default function Weather() {
   return (
     <div className="weatherApp">
       <div className="row">
+        <form>
+          <input
+            type="search"
+            placeholder="Search..."
+            classname="form-control"
+          />
+          <input type="submit" value="Search" className="btn btn-primary" />
+        </form>
+      </div>
+      <div className="row">
         <div className="col-6">
           <img
             src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
             alt="Mostly Cloudy"
           />
-          77°F
-          <ul>
+          <div className="currentTemp">77</div>
+          <p>°F</p>
+          <ul className="data">
             <li>Precipitation:15%</li>
             <li>Humidity:68%</li>
             <li>Wind:14mph</li>
@@ -19,8 +30,8 @@ export default function Weather() {
         </div>
 
         <div className="col-6">
-          <h1>City</h1>
-          <ul>
+          <h1>West Palm Beach</h1>
+          <ul className="currentData">
             <li>Wednesday 07:00</li>
             <li>Mostly Cloudy</li>
           </ul>
