90d47b19458f893499f17bd616504ceca5fea811
aivxx <90653195+aivxx@users.noreply.github.com>
2022-01-09 14:47:58 -0500

    added website and github url

--- src/App.js
+++ src/App.js
@@ -3,5 +3,23 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was coded by{" "}
+        <a href="https://ashleyrebeccaxr.com/" target="_blank">
+          {" "}
+          Ashley Goldstein
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/aivxx/SheCodes-Weather-React"
+          target="_blank"
+        >
+          open-sourced.
+        </a>
+      </footer>
+    </div>
+  );
 }
