03afd634e304a1ee9ad9a7c601d6ae1ef933242d
aivxx <90653195+aivxx@users.noreply.github.com>
2022-01-09 20:45:23 -0500

    import animated icons

--- src/weatherInfo.js
+++ src/weatherInfo.js
@@ -1,5 +1,6 @@
 import React from "react";
 import FormatDate from "./FormateDate";
+import WeatherIcon from "./WeatherIcon";
 
 export default function WeatherInfo(props) {
   return (
@@ -7,7 +8,8 @@ export default function WeatherInfo(props) {
       {" "}
       <div className="row">
         <div className="col-6">
-          <img src={props.data.iconUrl} alt={props.data.description} />
+          <WeatherIcon code={props.data.icon} alt={props.data.description} />
+
           <div className="currentTemp">
             {Math.round(props.data.temperature)}
           </div>
